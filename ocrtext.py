98d903bd06c094feeefe682f70928fdfff1aacdb
Mxtqnt <anacarolinass.cc@gmail.com>
2023-07-04 19:55:52 -0300

    easyocr

--- ocrtext.py
+++ ocrtext.py
@@ -1,65 +1,26 @@
-# Import required packages
+import easyocr
 import cv2
-import pytesseract
 
-# Mention the installed location of Tesseract-OCR in your system
-pytesseract.pytesseract.tesseract_cmd = 'System_path_to_tesseract.exe'
+image = cv2.imread('zoomcokie.png')
 
-# Read image from which text needs to be extracted
-img = cv2.imread("amostra.jpeg")
+reader = easyocr.Reader(['pt', 'pt'], gpu=True)
+result = reader.readtext(image)
 
-# Preprocessing the image starts
+new_image = image
 
-# Convert the image to gray scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if len(result) <= 0:
+    print('Nenhuma palavra encontrada')
+    exit(1)
 
-# Performing OTSU threshold
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+for word in result:
+    # print(word)
+    print(word[1], word[0])
+    start_point = (int(word[0][0][0]), int(word[0][0][1]))
+    end_point = (int(word[0][2][0]), int(word[0][2][1]))
+    color = (0, 0, 255)
+    thickness = 2
 
-# Specify structure shape and kernel size.
-# Kernel size increases or decreases the area
-# of the rectangle to be detected.
-# A smaller value like (10, 10) will detect
-# each word instead of a sentence.
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+    new_image = cv2.rectangle(new_image, start_point, end_point, color, thickness)
 
-# Applying dilation on the threshold image
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
-
-# Finding contours
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-												cv2.CHAIN_APPROX_NONE)
-
-# Creating a copy of image
-im2 = img.copy()
-
-# A text file is created and flushed
-file = open("recognized.txt", "w+")
-file.write("")
-file.close()
-
-# Looping through the identified contours
-# Then rectangular part is cropped and passed on
-# to pytesseract for extracting text from it
-# Extracted text is then written into the text file
-for cnt in contours:
-	x, y, w, h = cv2.boundingRect(cnt)
-	
-	# Drawing a rectangle on copied image
-	rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	
-	# Cropping the text block for giving input to OCR
-	cropped = im2[y:y + h, x:x + w]
-	
-	# Open the file in append mode
-	file = open("recognized.txt", "a")
-	
-	# Apply OCR on the cropped image
-	text = pytesseract.image_to_string(cropped)
-	
-	# Appending the text into file
-	file.write(text)
-	file.write("\n")
-	
-	# Close the file
-	file.close
+cv2.imshow('Palavras encontradas', new_image)
+cv2.waitKey(0)
