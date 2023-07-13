76953a3b191f1b84320a08c38f845b67e0425b5d
mxtqnt <anacarolinass.cc@gmail.com>
2023-07-13 13:57:04 -0300

    Cordenadas

--- pretos.py
+++ pretos.py
@@ -17,8 +17,15 @@ cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 for c in cnts:
     x,y,w,h = cv2.boundingRect(c)
     cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,255), 2)
+    cv2.putText(image, ("("+str(x)+", "+str(y)+")"), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
+    cv2.putText(image, ("("+str(x + w)+", "+str(y + h)+")"), (x + w, y + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
+    
+    # Cropping an image
+    cropped_image = image[y:(y+h), x:(x+w)]
+    # Display cropped image
+    cv2.imshow("cropped", cropped_image)
 
-cv2.imshow('thresh', thresh)
-cv2.imshow('dilate', dilate)
+#cv2.imshow('thresh', thresh)
+#cv2.imshow('dilate', dilate)
 cv2.imshow('image', image)
 cv2.waitKey()
