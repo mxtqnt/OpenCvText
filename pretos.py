54bf0f6b043be0537acfaa471a8b1d56d21fe7c3
mxtqnt <anacarolinass.cc@gmail.com>
2023-07-14 13:28:18 -0300

    feat: Encaminhando resultado para arquivo .txt

--- pretos.py
+++ pretos.py
@@ -1,5 +1,9 @@
+#-*- coding:utf-8 -*-
 import cv2
 import numpy as np
+import easyocr
+
+open('result.txt', 'w').close()
 
 # Load image, grayscale, Gaussian blur, Otsu's threshold
 image = cv2.imread('testes.png')
@@ -16,14 +20,30 @@ cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 for c in cnts:
     x,y,w,h = cv2.boundingRect(c)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,255), 2)
-    cv2.putText(image, ("("+str(x)+", "+str(y)+")"), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
-    cv2.putText(image, ("("+str(x + w)+", "+str(y + h)+")"), (x + w, y + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
+    #cv2.rectangle(image, (x, y), (x + w, y + h), (255,0,255), 2)
+    #cv2.putText(image, ("("+str(x)+", "+str(y)+")"), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
+    #cv2.putText(image, ("("+str(x + w)+", "+str(y + h)+")"), (x + w, y + h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 100), 2)
     
     # Cropping an image
     cropped_image = image[y:(y+h), x:(x+w)]
-    # Display cropped image
-    cv2.imshow("cropped", cropped_image)
+    reader = easyocr.Reader(['pt', 'pt'], gpu=False)
+    result = reader.readtext(cropped_image, paragraph="False", detail='False')
+
+    new_image = cropped_image
+
+    if len(result) <= 0:
+        print('Nenhuma palavra encontrada')
+        exit(1)
+
+    for res in result:
+        top_left = (int(res[0][0][0]), int(res[0][0][1])) # convert float to int
+        bottom_right = (int(res[0][2][0]), int(res[0][2][1])) # convert float to int
+        #cv2.putText(cropped_image, res[1], (top_left[0], top_left[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 255), 1)
+        for item in result:
+            with open('result.txt', 'a', encoding="UTF-8") as f:
+                f.write(item[1])
+                f.write('\n')
+            #print(item[1])
 
 #cv2.imshow('thresh', thresh)
 #cv2.imshow('dilate', dilate)
